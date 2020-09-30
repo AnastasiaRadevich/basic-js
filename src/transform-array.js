b045438da7f9c4ff1b5887a51b9a5b084a661181
Radevich <n.radievich@mail.ru>
2020-09-30 20:17:40 +0300

    feat: add task transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    if (!(Array.isArray(arr))) throw new Error;
+    
+    let d;
+
+    let newArr = []
+
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i] === `--discard-next`) {
+                    d = i + 1;
+                    i++;
+                } else if (arr[i] === `--discard-prev`) {
+                    let remove = newArr.splice(-1, 1);
+                } else if (arr[i] === `--double-next`) {
+                    newArr.push(arr[++i])
+                    i--;
+                } else if (arr[i] === `--double-prev`) { if (d !== i - 1) 
+                    newArr.push(arr[--i])
+                } else {
+                     if (d !== i + 1) {
+                    newArr.push(arr[i])}
+                } 
+                }
+            
+            return newArr;
+}
+
