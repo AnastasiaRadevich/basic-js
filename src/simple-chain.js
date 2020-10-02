7a076edae81665dc51bb5d9ab1fa7b3f4f03bac9
Radevich <n.radievich@mail.ru>
2020-10-02 17:00:02 +0300

    feat: add task simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,47 @@
 const CustomError = require("../extensions/custom-error");
 
-const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+let chainMaker = {
+    chain: [],
+
+    getLength() {
+
+        return this.chain.length;
+    },
+    addLink(value) {
+
+        this.value = value
+
+        const link = `( ${value} )`;
+
+        this.chain.push(link);
+
+        return this;
+
+    },
+
+    removeLink(position) {
+
+        this.position = position;
+        if (position > 0 && position <= this.chain.length) {
+            this.chain.splice(position - 1, 1)
+        } else {
+            this.chain = [];
+            throw new Error;
+        }
+        return this
+
+    },
+    reverseChain() {
+        this.chain = this.chain.reverse();
+        return this;
+
+    },
+    finishChain() {
+        let finishChain = this.chain.join('~~');
+        this.chain = [];
+        return finishChain;
+
+    }
 };
 
 module.exports = chainMaker;
